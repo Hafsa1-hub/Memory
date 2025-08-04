0e7769437d26804e0ad661419c59e5d7134822f2
Hafsa1-hub <trainee4@vaaluka.com>
2025-08-04 15:26:04 +0530

    version2

--- axi_master.sv
+++ axi_master.sv
@@ -1,8 +1,8 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                         AXI MASTER 
+//                         AXI MASTER
 
 
-// ---------------         Global signal    -------------------------------------------------------------//	
+// ---------------         Global signal    -------------------------------------------------------------//
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ACLK         -> Signals are sampled on the rising edge of the global clock.                        //
 // ARESETn      -> Reset signal is active LOW                                                         //
@@ -43,13 +43,13 @@
 ////---------------------- AXI read address channel signals.------------------------------------------//
 
 // /////////////////////////////////////////////////////////////////////////////////////////////////////
-// ARID[3:0]    -> Master Read address ID                                                              // 
+// ARID[3:0]    -> Master Read address ID                                                              //
 // ARADDR[31:0] -> Master Read address. .                                                              //
 // ARLEN[3:0]   -> Master Burst length.                                                                //
 // ARSIZE[2:0]  -> Master Burst size.                                                                  //
-// ARBURST[1:0] -> Master Burst type.                                                                  // 
+// ARBURST[1:0] -> Master Burst type.                                                                  //
 // ARVALID      -> Master Read address                                                                 //
-// ARREADY,     -> SLAVE is high.                                                                            //
+// ARREADY,     -> SLAVE is high.                                                                      //
 // /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //-------------------------Read data channel signals -------------------------------------------------//
@@ -62,50 +62,223 @@
 // RVALID       -> Slave Read valid.                                                                   //
 // RREADY       -> Master is ready                                                                     //
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//--------------------------------------------------------------------------------------------------------//
-module axi_slave 
-//-----global signal-----------------//
-  input aclk_i         ,
-  input areset_i       ,
-  //-WRITE ADDRESS CHANNEL-----------//
-  input awid    [3:0]  ,    
-  input awaddr  [31:0] , 
-  input awlen   [3:0]  ,
-  input awsize  [2:0]  ,
-  input awburst [1:0]  ,
-  input awvalid        ,
-  output reg awready   ,
+
+//-----------------------------------------------------------------------------------------------------//
+
+//-----------------------------------------------------------------------------------------------------//
+class traffic_generator;
+
+  //----------------------------//
+  rand reg     [ 3:0] awid_i;
+  rand reg     [31:0] awaddr_i;
+  rand reg     [ 3:0] awlen_i;
+  rand reg     [ 2:0] awsize_i;
+  rand reg     [ 1:0] awburst_i;
+  rand reg            awvalid_i;
+
+  //----------------------------//
+  rand reg     [ 3:0] wid_i;
+  rand reg     [31:0] wdata_i;
+  rand reg     [ 3:0] wstrb_i;
+  rand reg            wlast_i;
+  rand reg            wvalid_i;
+
+  //----------------------------//
+  rand reg            bready_i;
+
+  //----------------------------//
+  rand reg     [ 3:0] arid_i;
+  rand reg     [31:0] araddr_i;
+  rand reg     [ 3:0] arlen_i;
+  rand reg     [ 2:0] arsize_i;
+  rand reg     [ 1:0] arburst_i;
+  rand reg            arvalid_i;
+
+  //----------------------------//
+  rand reg            rready_i;
+
+  //----------------------------//
+  rand reg     [ 3:0] rid_o;
+  rand reg     [31:0] rdata_o;
+  rand reg     [ 1:0] rresp_o;
+  rand reg            rlast_o;
+  rand reg            rvalid_o;
+
+  //----------------------------//
+  int unsigned        data_count;  // used in constraint
+
+  //-------------------------- Write Address Channel --------------------------//
+  constraint write_address_channel_c {
+    awlen_i inside {[0 : 15]};
+    awsize_i inside {3'b001};  // size = 2 bytes
+    awvalid_i == 1;
+    awburst_i == 2'b01;  //Incr
+    awaddr_i inside {32'h2, 32'h4, 32'h8};  // aligned address values
+  }
+
+  //-------------------------- Write Data Channel -----------------------------//
+  constraint write_data_channel_c {
+    wid_i inside {[1 : 3]};
+    wvalid_i == 1;
+    wlast_i == ((data_count == awlen_i) ? 1 : 0);
+    awsize_i inside {3'b000};  // size = 1 byte
+    wstrb_i == (1 << (1 << awsize_i)) - 1;
+  }
+
+  //-------------------------- Write Response Channel --------------------------//
+  constraint write_response_channel_c {bready_i == 1;}
+
+  //-------------------------- Read Address Channel ----------------------------//
+  constraint read_address_channel_c {
+    araddr_i inside {32'h2, 32'h4, 32'h8};
+    arid_i inside {[1 : 3]};
+    arlen_i inside {3};
+    arsize_i inside {3'b000};
+    arburst_i == 2'b01;  // INCR burst
+    arvalid_i == 1;
+  }
+
+  //-------------------------- Read Data Channel ------------------------------//
+  constraint read_data_channel_c {rready_i == 1;}
+
+endclass
+
+
+//---------------------------------------------------------------------------------------------------------------------//
+module axi_master (
+    //-----global signal-----------------//
+    output reg        aclk_o,
+    output reg        areset_o,
+    //-WRITE ADDRESS CHANNEL-----------//
+    output reg [ 3:0] awid_o,
+    output reg [31:0] awaddr_o,
+    output reg [ 3:0] awlen_o,
+    output reg [ 2:0] awsize_o,
+    output reg [ 1:0] awburst_o,
+    output reg        awvalid_o,
+    input             awready_i,
+    //-WRITE DATA CHANNEL--------------//
+    output reg [ 3:0] wid_o,
+    output reg [31:0] wdata_o,
+    output reg [ 3:0] wstrb_o,
+    output reg        wlast_o,
+    output reg        wvalid_o,
+    input             wready_i,
+
+    // WRITE RESPONSE CHANNEL----------//
+    input      [3:0] bid_i,
+    input      [1:0] bresp_i,
+    input            bvalid_i,
+    output reg       bready_o,
+
+    ///READ ADDRESS CHANNEL  ----------//
+    output reg [ 3:0] arid_o,
+    output reg [31:0] araddr_o,
+    output reg [ 3:0] arlen_o,
+    output reg [ 2:0] arsize_o,
+    output reg [ 1:0] arburst_o,
+    output reg        arvalid_o,
+    input             arready_i,
+    // READ DATA CHANNEL---------------//
+    input      [ 3:0] rid_i,
+    input      [31:0] rdata_i,
+    input      [ 1:0] rresp_i,
+    input             rlast_i,
+    input             rvalid_i,
+    output reg        rready_o
+);
+  //write_data_transfer txn;
+  //-----------------Clock Genaration -------------
+  int data_count;
+  traffic_generator tg;
   
-  //-WRITE DATA CHANNEL--------------//
-  input wid     [3:0]  ,
-  input wdata   [31:0] ,
-  input wstrb   [3:0]  ,
-  input wlast          ,
-  input wvalid         ,
-  input wready         ,
+  initial begin
+    aclk_o = 0;
+    tg = new();
+    data_count = 0;
+  end
 
-  // WRITE RESPONSE CHANNEL----------//
-  input bid     [3:0]  ,    
-  input bresp   [1:0]  ,
-  input bvalid         ,
-  input bready         ,
-  ///READ ADDRESS CHANNEL  ----------//
-  input arid    [3:0 ] ,
-  input araddr  [31:0] ,
-  input arlen   [3:0 ] ,
-  input arsize  [2:0 ] ,
-  input arburst [1:0 ] ,
-  input arvalid        ,
-  input arready        ,
-  // READ DATA CHANNEL---------------//
-  input rid     [3:0 ] ,
-  input rdata   [31:0] ,
-  input rresp   [1:0 ] ,
-  input rlast          ,
-  input rvalid         ,
-  input rready         ,
-  //---------------------------------//
+  always begin
+    #5 aclk_o = (~aclk_o);
+  end
+  //-----------------------------------------------
+  always @(posedge aclk_o) begin
+      if(areset_o==0 ) begin 
+         awid_o    <= 'dx;  
+         awaddr_o  <= 'dx;
+         awlen_o   <= 'dx;
+         awsize_o  <= 'dx;
+         awburst_o <= 'dx;
+         awvalid_o <= 'dx;
+         wid_o     <= 'dx;
+         wdata_o   <= 'dx;
+         wstrb_o   <= 'dx;
+         wlast_o   <= 'dx;
+         wvalid_o  <= 'dx;
+         arid_o    <= 'dx;
+         araddr_o  <= 'dx;
+         arlen_o   <= 'dx;
+         arsize_o  <= 'dx;
+         arburst_o <= 'dx;
+         arvalid_o <= 'dx;
+         bready_o  <= 'dX;
+         rready_o  <= 'dx;
+      end
+      else begin
+    //------------------address channel---------------//
+      if (tg.randomize()) begin
+      $display("Randomizion for 1st ");
+      if (awready_i) begin
+        $strobe("Slave is ready to accept the address from write address channel");
+        awid_o    <=  tg.awid_i   ;
+        awaddr_o  <=  tg.awaddr_i ;
+        awlen_o   <=  tg.awlen_i  ;
+        awsize_o  <=  tg.awsize_i ;
+        awburst_o <=  tg.awburst_i;
+        awvalid_o <=  tg.awvalid_i;
+      end
+      if (wready_i) begin
+        tg.write_address_channel_c.constraint_mode(0);
+        wid_o    <= tg.wid_i;
+        wdata_o  <= tg.wdata_i;
+        wstrb_o  <= tg.wstrb_i;
+        wlast_o  <= tg.wlast_i;
+        wvalid_o <= tg.wvalid_i;
+       $strobe("Slave is ready to accept the data from write data channel");
+       data_count <= 0;
+       tg.write_address_channel_c.constraint_mode(0);  // transfer all complete transcati
+       for (data_count = 0; data_count <= awlen_o; data_count++) begin
+        //   $display("awaddr_i = %d, wvalid_i = %0d, wstrb_i = %b", tg.awaddr_i, tg.wvalid_i,
+        //           tg.wstrb_i);
+        data_count <= data_count + 1;
+        @(posedge aclk_o) data_count <= data_count + 1;
+      end
+    end
+    bready_o <= 1;
+    $strobe("The write Data response for the data is\t BID :: %d\t  bresp::%d\t  bavalid_i :: %d\t",
+            bid_i, bresp_i, bvalid_i);
+  //end
+  //--------------READ Data OPERATION--------------------------------------//
+    if (arready_i) begin
+        arid_o    <= tg.arid_i;
+        araddr_o  <= tg.araddr_i;
+        arlen_o   <= tg.arlen_i;
+        arsize_o  <= tg.arsize_i;
+        arburst_o <= tg.arburst_i;
+        arvalid_o <= tg.arvalid_i;
+    end
+    if (rready_o) begin // Read address
+$stobe("the received address response for\n::id is%d\t data is %d\t resp ::%d\t lst_i
+                  ::%d\t rvalid ::%d\t",rid_i ,rdata_i, rresp_i,rlast_i,rvalid_i);
+    end
+    else begin
+        $strobe("response is not yet received");
+    end
+  end
+end
+end
+    
+   endmodule
 
 
 
@@ -115,28 +288,6 @@ module axi_slave
 
 
 
-//--------SLAVE signal----------------//
-  output reg       aw_ready_o,
-  output reg       wready_o,
-  output reg [3:0] bid_o,
-  output reg [1:0] bresp,
-  output reg       bvalid,
-  output reg       arready
-  output reg       rid_o,
-  output reg [1:0] rresp,
-  output reg       rvalid,
-  output reg [31:0]rdata_o,
-  output reg       rlast,
 
- AWREADY 
- WREADY
-  BID[3:0]  
-  BRESP[1:0]
-  BVALID
-  ARREADY,o/p
-   RID[3:0]    
-   RDATA[31:0] 
-   RRESP[1:0]  
-   RLAST       
-   RVALID      
+
 
--- hamming_code.sv
+++ hamming_code.sv
@@ -31,30 +31,130 @@
 //              | P1 | P2 | D1 | P4 | D2 | D3 | D4 |
 // ///////////////////////////////////////////////////////////////////////////////////////////////  
 
-module encoder_hamming_code(
-                     input [7:0] data_i,
-                     output reg [7:0] data_o
-                   );
+// Hamming (12,8) Encoder
+module hamming_encoder (
+  input  logic [7:0] data_i,
+  output logic [11:0] code_o
+);
+  logic p1, p2, p4, p8;
 
-// SYNDROME BITS
-  reg [7:0] flip_bit;
-  reg s1;
-  reg s2;
-  reg s3;
-  reg [2:0] error_bit;
-//------------------------------------S1 = P1 ^ D1 ^ D2 ^ D4 
-  //initial begin
-  always @(*) begin
-    s1 = data_i[0]^data_i[2]^data_i[4]^data_i[6];
-    s2 = data_i[1]^data_i[2]^data_i[5]^data_i[6];
-    s3 = data_i[3]^data_i[4]^data_i[5]^data_i[6];
-    error_bit = {s1,s2,s3};
-    $display ("the syndrome bit a= %d\t we need to flip that number ",error_bit);
-    data_o =  data_i ;
-    data_o[error_bit] = (~data_i[error_bit]);
-    $display ("the actual input data is %b\n the error corrected data after fliping the bit is %b\n",data_i,data_o);
-    end
+  always_comb begin
+    code_o[2]  = data_i[7]; // position 3
+    code_o[4]  = data_i[6]; // position 5
+    code_o[5]  = data_i[5]; // position 6
+    code_o[6]  = data_i[4]; // position 7
+    code_o[8]  = data_i[3]; // position 9
+    code_o[9]  = data_i[2]; // position 10
+    code_o[10] = data_i[1]; // position 11
+    code_o[11] = data_i[0]; // position 12
+
+    // parity bits
+    p1 = code_o[2] ^ code_o[4] ^ code_o[6] ^ code_o[8] ^ code_o[10];
+    p2 = code_o[2] ^ code_o[5] ^ code_o[6] ^ code_o[9] ^ code_o[10];
+    p4 = code_o[4] ^ code_o[5] ^ code_o[6] ^ code_o[11];
+    p8 = code_o[8] ^ code_o[9] ^ code_o[10] ^ code_o[11];
+
+    code_o[0] = p1; // position 1
+    code_o[1] = p2; // position 2
+    code_o[3] = p4; // position 4
+    code_o[7] = p8; // position 8
+  end
 endmodule
+
+
+// Hamming (12,8) Decoder
+module hamming_decoder (
+  input  logic [11:0] code_i,
+  output logic [7:0] data_o,
+  output logic       error_detected
+);
+  logic [3:0] syndrome;
+  logic [11:0] corrected;
+
+  always_comb begin
+    syndrome[0] = code_i[0] ^ code_i[2] ^ code_i[4] ^ code_i[6] ^ code_i[8] ^ code_i[10];
+    syndrome[1] = code_i[1] ^ code_i[2] ^ code_i[5] ^ code_i[6] ^ code_i[9] ^ code_i[10];
+    syndrome[2] = code_i[3] ^ code_i[4] ^ code_i[5] ^ code_i[6] ^ code_i[11];
+    syndrome[3] = code_i[7] ^ code_i[8] ^ code_i[9] ^ code_i[10] ^ code_i[11];
+
+    corrected = code_i;
+    error_detected = (syndrome != 4'd0);
+
+    if (error_detected) begin
+      corrected[syndrome - 1] = ~corrected[syndrome - 1];
+    end
+
+    data_o = { corrected[2], corrected[4], corrected[5], corrected[6],
+               corrected[8], corrected[9], corrected[10], corrected[11] };
+  end
+endmodule
+
+
+// Simple Testbench
+module tb_hamming;
+  logic [7:0] data;
+  logic [11:0] encoded, corrupted;
+  logic [7:0] decoded;
+  logic error;
+
+  hamming_encoder enc(.data_i(data), .code_o(encoded));
+  hamming_decoder dec(.code_i(corrupted), .data_o(decoded), .error_detected(error));
+
+  initial begin
+    data = 8'b00110011;
+    #1;
+    $display("Original Data     = %b", data);
+    $display("Encoded Hamming   = %b", encoded);
+
+    // Inject 1-bit error at bit 6 (index 5)
+    corrupted = encoded;
+    corrupted[5] = ~corrupted[5];
+
+    #1;
+    $display("Corrupted Code    = %b", corrupted);
+    $display("Decoded Data      = %b", decoded);
+    $display("Error Detected?   = %b", error);
+    $finish;
+  end
+endmodule
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+module encoder_hamming_code(
+    input  [7:0] data_i,
+    output reg [7:0] data_o
+);
+
+// Internal variables
+reg s1, s2, s3;
+reg [2:0] error_bit;
+integer error_index;
+
+always @(*) begin
+    // Compute syndrome bits
+    s1 = data_i[0] ^ data_i[2] ^ data_i[4] ^ data_i[6];
+    s2 = data_i[1] ^ data_i[2] ^ data_i[5] ^ data_i[6];
+    s3 = data_i[3] ^ data_i[4] ^ data_i[5] ^ data_i[6];
+    error_bit = {s3, s2, s1};  // Note the order: MSB to LSB
+    error_index = error_bit;  // Convert to integer for indexing
+
+    data_o = data_i;
+
+    // Only flip if error index is non-zero and in range
+    if (error_index > 0 && error_index < 8) begin
+        data_o[error_index] = ~data_i[error_index];
+        $display("Error detected at bit %0d", error_index);
+    end else begin
+        $display("No error detected.");
+    end
+
+    $display("Original data: %b", data_i);
+    $display("Corrected data: %b", data_o);
+end
+
+endmodule
+
 
 module test;
   reg  [7:0] data_in;
@@ -66,7 +166,7 @@ module test;
                                    );
   initial begin
     data_in  = 'b0110001;
-    #15;
+    #10;
     data_in  = 'b1000110;
     #5;
     data_in   = 'b00110011;
